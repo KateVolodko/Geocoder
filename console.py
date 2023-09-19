fa7d9c97656772312d80aa7a9f265d2aff2dc8e4
Alexey <greenbelg@internet.ru>
2023-09-19 09:23:27 +0500

    Add line_profile files and refactor console's path

--- console.py
+++ console.py
@@ -1,11 +1,7 @@
 import click
-import pathlib
-import sys
 
-sys.path.append(str(pathlib.Path(__file__).parent.parent.parent.absolute()))
-
-from Geocoder.words_parser.finder import CoordinatesFinder
-from Geocoder.words_parser.address_parser import Address_parser
+from words_parser.finder import CoordinatesFinder
+from words_parser.address_parser import Address_parser
 
 
 @click.command()
